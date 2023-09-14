7920bb354d7e7873670edfcbca23635f2370636a
Laurrencia <elaurrencia27@gmail.com>
2023-09-14 18:01:56 +0400

    update header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import poster from '../images/poster.png';
 import logo from '../images/tv.png';
+import menu from '../images/menu-alt-4-1.svg';
+import {FaSearch} from 'react-icons/fa';
+
 
 
 function Header() {
@@ -9,12 +12,21 @@ function Header() {
     <div style={{backgroundImage: `url(${poster})`}} className='header-fluid'>
       <div className='navbar'>
         <div className='logo'>
-          <img src={logo} alt='tvimg' className='tv' width="100px" height="100px"/>
+          <img src={logo} alt='tvimg' className='tv' width="20px" height="20px"/>
           <h4 className='logoName'>Moviebox</h4>
-
         </div>
-        <div>
-          <input type='search'/>
+        <div className='search'>
+          <input 
+            type='text'
+            placeholder='What do you want to watch?'
+            className='input'
+          />
+          <FaSearch className='search-logo'/>  
+        </div>
+        <div className='menu'>
+          <h4 className='signIn'>Sign In</h4>
+          <img src={menu} alt='menu-img' className='menuBtn' width="20px" height="20px"/>
+
         </div>
 
       </div>
